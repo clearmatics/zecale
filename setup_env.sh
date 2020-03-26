bd48534a8f5c99d8a12e87d8a02974be43714e78
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-03-26 17:00:47 +0000

    Removed trusted_setup folder from git flow and updated setup script accordingly

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 export ZECALE=`pwd`
-export ZECALE_TRUSTED_SETUP_DIR=$ZECALE/trusted_setup
 export ZECALE_DEBUG_DIR=$ZECALE/debug
 export ZECALE_API_DIR=$ZECALE/api
+
+mkdir -p $ZECALE/trusted_setup
+export ZECALE_TRUSTED_SETUP_DIR=$ZECALE/trusted_setup
 
 # Add the zecale executables in the PATH
 export PATH=$ZECALE/build/src:$PATH
