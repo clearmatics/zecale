c15af550e0aa4bd81e9c434b66f0f0ae8642db13
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-10-27 14:19:52 +0000

    Renamed setup dir env var and added setup dir to gitignore

--- setup_env.sh
+++ setup_env.sh
@@ -3,8 +3,8 @@
 export ZECALE=`pwd`
 export ZECALE_DEBUG_DIR=$ZECALE/debug
 
-mkdir -p $ZECALE/trusted_setup
-export ZECALE_TRUSTED_SETUP_DIR=$ZECALE/trusted_setup
+export ZECALE_SETUP_DIR=$ZECALE/zecale_setup
+mkdir -p $ZECALE_SETUP_DIR
 
 # Add the zecale executables in the PATH
 export PATH=$ZECALE/build/src:$PATH
