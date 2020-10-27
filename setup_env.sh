60993f6226e054baab4a6b5e350e0d2d2b8a09d1
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-10-27 14:28:17 +0000

    Fixed path to executable in setup script

--- setup_env.sh
+++ setup_env.sh
@@ -7,4 +7,4 @@ export ZECALE_SETUP_DIR=$ZECALE/zecale_setup
 mkdir -p $ZECALE_SETUP_DIR
 
 # Add the zecale executables in the PATH
-export PATH=$ZECALE/build/src:$PATH
+export PATH=$ZECALE/build/aggregator_server:$ZECALE/build/libzecale/tests:$PATH
