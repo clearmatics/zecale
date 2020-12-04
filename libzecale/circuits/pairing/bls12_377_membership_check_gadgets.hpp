790df8db01e7fc47ec24cdc4664d4cd507c1822d
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-12-04 15:31:26 +0000

    libzecale: bls12_377 g2 membership check gadget

--- libzecale/circuits/pairing/bls12_377_membership_check_gadgets.hpp
+++ libzecale/circuits/pairing/bls12_377_membership_check_gadgets.hpp
@@ -11,6 +11,7 @@
 
 #include <libff/algebra/curves/bls12_377/bls12_377_init.hpp>
 #include <libsnark/gadgetlib1/gadgets/curves/weierstrass_g1_gadget.hpp>
+#include <libsnark/gadgetlib1/gadgets/curves/weierstrass_g2_gadget.hpp>
 
 namespace libzecale
 {
@@ -50,6 +51,41 @@ libsnark::G2_variable<wppT> bls12_377_g2_untwist_frobenius_twist(
     const libsnark::G2_variable<wppT> &g2,
     size_t exp,
     const std::string &annotation_prefix);
+
+/// Curve equation and subgroup membership check for BLS12-377 G2 variables.
+template<typename wppT>
+class bls12_377_G2_membership_check_gadget : libsnark::gadget<libff::Fr<wppT>>
+{
+public:
+    // Follows libff implementation of bls12_377_G2::is_in_safe_subgroup().
+    // See: libff/algebra/curves/bls12_377/bls12_377_g2.cpp.
+
+    // Check that[h1.r] P == 0, where
+    //   [h1.r]P is P + [t](\psi(P) - P) - \psi^2(P)
+    // (See bls12_377.sage).
+    // Note that in this case we check that:
+    //   P + [t](\psi(P) - P) = \psi^2(P)
+    // since G2_variable cannot represent 0 (in G2).
+
+    // Check P is well-formed
+    libsnark::G2_checker_gadget<wppT> _P_checker;
+    // \psi(P) - P
+    G2_add_gadget<wppT> _psi_P_minus_P;
+    // [t](\psi(P) - P)
+    G2_mul_by_const_scalar_gadget<wppT, libff::bls12_377_r_limbs>
+        _t_times_psi_P_minus_P;
+    // P + [t](\psi(P) - P)
+    G2_add_gadget<wppT> _P_plus_t_times_psi_P_minus_P;
+    // P + [t](\psi(P) - P) = \psi^2(P)
+    G2_equality_gadget<wppT> _h1_r_P_equals_zero;
+
+    bls12_377_G2_membership_check_gadget(
+        libsnark::protoboard<libff::Fr<wppT>> &pb,
+        libsnark::G2_variable<wppT> &g2,
+        const std::string &annotation_prefix);
+    void generate_r1cs_constraints();
+    void generate_r1cs_witness();
+};
 
 } // namespace libzecale
 
