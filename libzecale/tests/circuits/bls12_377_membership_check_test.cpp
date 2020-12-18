e52524c546bf3e5dd8fca2940c75d3af3080ae5c
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-12-18 12:04:22 +0000

    WIP: libzeth: test for group element well-formedess checker (in libsnark)

--- libzecale/tests/circuits/bls12_377_membership_check_test.cpp
+++ libzecale/tests/circuits/bls12_377_membership_check_test.cpp
@@ -19,6 +19,22 @@ using Field = libff::Fr<wpp>;
 namespace
 {
 
+void g1_well_formed_check_circuit(const libff::G1<npp> &g1_value)
+{
+    ASSERT_TRUE(g1_value.is_well_formed());
+
+    libsnark::protoboard<Field> pb;
+    libsnark::G1_variable<wpp> g1(pb, " g1");
+    libsnark::G1_checker_gadget<wpp> check_g1(pb, g1, "check_g1");
+
+    check_g1.generate_r1cs_constraints();
+
+    g1.generate_r1cs_witness(g1_value);
+    check_g1.generate_r1cs_witness();
+
+    ASSERT_EQ(g1_value.is_well_formed(), pb.is_satisfied());
+}
+
 void g1_membership_check_circuit(const libff::G1<npp> &g1_value)
 {
     ASSERT_TRUE(g1_value.is_well_formed());
@@ -33,7 +49,9 @@ void g1_membership_check_circuit(const libff::G1<npp> &g1_value)
     g1.generate_r1cs_witness(g1_value);
     check_g1.generate_r1cs_witness();
 
-    ASSERT_EQ(g1_value.is_in_safe_subgroup(), pb.is_satisfied());
+    ASSERT_EQ(
+        g1_value.is_well_formed() && g1_value.is_in_safe_subgroup(),
+        pb.is_satisfied());
 }
 
 void g2_membership_check_circuit(const libff::G2<npp> &g2_value)
@@ -50,7 +68,9 @@ void g2_membership_check_circuit(const libff::G2<npp> &g2_value)
     g2.generate_r1cs_witness(g2_value);
     check_g2.generate_r1cs_witness();
 
-    ASSERT_EQ(g2_value.is_in_safe_subgroup(), pb.is_satisfied());
+    const bool expect_result =
+        g2_value.is_well_formed() && g2_value.is_in_safe_subgroup();
+    ASSERT_EQ(expect_result, pb.is_satisfied());
 }
 
 TEST(BLS12_377_Membership_Check, G1ValidMember)
@@ -63,6 +83,22 @@ TEST(BLS12_377_Membership_Check, G1InvalidMember)
 {
     const libff::G1<npp> g1_invalid =
         libff::g1_curve_point_at_x<libff::G1<npp>>(libff::Fq<npp>(3));
+    g1_membership_check_circuit(g1_invalid);
+}
+
+TEST(BLS12_377_Membership_Check, G1NotWellFormedGadget)
+{
+    libff::Fq<npp> two = libff::Fq<npp>::one() + libff::Fq<npp>::one();
+    const libff::G1<npp> g1_invalid(two, two, libff::Fq<npp>::one());
+    ASSERT_FALSE(g1_invalid.is_well_formed());
+    g1_well_formed_check_circuit(g1_invalid);
+}
+
+TEST(BLS12_377_Membership_Check, G1NotWellFormed)
+{
+    const libff::G1<npp> g1_invalid(
+        libff::Fq<npp>::one(), libff::Fq<npp>::one(), libff::Fq<npp>::one());
+    ASSERT_FALSE(g1_invalid.is_well_formed());
     g1_membership_check_circuit(g1_invalid);
 }
 
@@ -95,6 +131,14 @@ TEST(BLS12_377_Membership_Check, G2InvalidMember)
     const libff::G2<npp> g2_invalid =
         libff::g2_curve_point_at_x<libff::G2<npp>>(
             libff::Fq<npp>(3) * libff::Fqe<npp>::one());
+    g2_membership_check_circuit(g2_invalid);
+}
+
+TEST(BLS12_377_Membership_Check, G2NotWellFormed)
+{
+    const libff::G2<npp> g2_invalid(
+        libff::Fqe<npp>::one(), libff::Fqe<npp>::one(), libff::Fqe<npp>::one());
+    ASSERT_FALSE(g2_invalid.is_well_formed());
     g2_membership_check_circuit(g2_invalid);
 }
 
