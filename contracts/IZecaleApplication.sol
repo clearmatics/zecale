e0bab3fe1c844ba67a50be69b9a4d2032a12c829
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-02-09 16:45:11 +0000

    update solidity compiler (including zeth submodule)

--- contracts/IZecaleApplication.sol
+++ contracts/IZecaleApplication.sol
@@ -2,9 +2,11 @@
 //
 // SPDX-License-Identifier: LGPL-3.0+
 
+pragma solidity ^0.8.0;
+
 // Interface that must be implemented by the contract of application using
 // Zecale.
-contract IZecaleApplication
+abstract contract IZecaleApplication
 {
     // The main entry-point called by Zecale. Implementations should ensure
     // that this is called by a trusted implementation of the Zecale
@@ -21,5 +23,9 @@ contract IZecaleApplication
     function dispatch(
         uint256[2] memory nested_vk_hash,
         uint256[] memory nested_inputs,
-        bytes memory nested_parameters) public payable;
+        bytes memory nested_parameters
+    )
+        public
+        payable
+        virtual;
 }
